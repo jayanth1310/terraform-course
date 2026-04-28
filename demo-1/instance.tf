289e7db7e9c0aea0447eca5d544c20f5b1183fbb
jayanth1310 <90104768+jayanth1310@users.noreply.github.com>
2026-04-28 22:24:03 +0530

    sg added

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -3,3 +3,22 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
 }
 
+resource "aws_security_group" "sg_security_private_allow_ssh"{
+  vpc_id = "${aws_vpc.main.id}"
+  name = "allow_ssh"
+  egress = {
+    to_port = 0
+    from_port = 0
+    protocol = -1
+    cidr_block = ["0.0.0.0/0"]
+  }
+  ingress = {
+    to_port = 22
+    from_port = 22
+    protocol = -1
+    cidr_block = ["0.0.0.0/0]
+  }
+  tags={
+    name = "sg for inward and outward of data"
+  }
+}
